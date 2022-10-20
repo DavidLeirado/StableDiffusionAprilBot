2ae3ebd1588665b26ce4f699f1c273cdce6ee1a4
David Leirado Maroto <dleiradomaroto@gmail.com>
2022-10-20 22:16:35 +0200

    feat: sd to cpu

--- sd_implementation.py
+++ sd_implementation.py
@@ -1,11 +1,11 @@
-import os
-import sys
 import io
+import os
 
 import torch
 from diffusers import StableDiffusionPipeline
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Response
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 
 
@@ -16,11 +16,17 @@ class Prompt(BaseModel):
 load_dotenv()
 TOKEN = os.environ.get("SD_TOKEN", "")
 
-pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=TOKEN)
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=TOKEN,
+                                               torch_dtype=torch.float16, revision="fp16")
 device = "cuda" if torch.cuda.is_available() else "cpu"
-pipe.to("cuda")
+pipe.to(device)
 
 app = FastAPI()
+
+
+@app.get("/")
+async def home():
+    return RedirectResponse("/docs", status_code=302)
 
 
 @app.post("/sdimage")
@@ -36,4 +42,3 @@ async def sd_image_gen(prompt_content: Prompt):
     image.save(buf, format("PNG"))
     image = buf.getvalue()
     return Response(content=image, media_type="image/png")
-
